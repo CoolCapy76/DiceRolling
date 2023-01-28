022f25dc70786a6647c1d59445e262ef44f7a2cb
CoolCapy76 <120516620+CoolCapy76@users.noreply.github.com>
2023-01-28 22:06:46 +0200

    Update DiceRolling.cpp

--- DiceRolling.cpp
+++ DiceRolling.cpp
@@ -3,20 +3,14 @@
 #include <conio.h>
 using namespace std;
 
-
-
 int rollDie()
 {
     int roll;
     int min = 1; // the min number a die can roll is 1
     int max = 6;// this->dieSize; // the max value is the die size
-
     roll = rand() % (max - min + 1) + min;
     return roll;
 }
-
-
-
 
 int main()
 {
@@ -31,11 +25,8 @@ Start:
             {
                 cout << rollDie() << endl;
                 
-
             }
             goto Start;
-            
-            
         }
         else
         {
